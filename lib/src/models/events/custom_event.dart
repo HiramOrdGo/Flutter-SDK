ee5f645721e86d06f1947d81abc444e5aa28dcc2
Hiram Ordoñez <hiram.ordonez@go1.mx>
2023-06-16 16:31:09 -0500

    feat (CustomEvent) reomve timespan to CustomEvent

--- lib/src/models/events/custom_event.dart
+++ lib/src/models/events/custom_event.dart
@@ -1,16 +1,14 @@
 class CustomEvent {
   final String event;
-  final DateTime timestamp;
   final Map<String, dynamic> data;
   CustomEvent({
     required this.event,
     required this.data,
-  }) : timestamp = DateTime.now();
+  });
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'event': event,
-      'timestamp': timestamp.toIso8601String(),
       'data': data,
     };
   }
